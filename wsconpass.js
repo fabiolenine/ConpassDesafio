60f13528283c58114b3c777e1669065bd3ee1382
Fabio Lenine <fabiolenine@gmail.com>
2018-01-12 22:25:50 -0300

    alter
    Implementando WS (início)

---
+++ wsconpass.js
@@ -0,0 +1,32 @@
+/*
+ * WSCONPASS - Conpass Desafio
+ *
+ * Author: Fabio Lenine Vilela da Silva
+ * LICENSE: MIT
+ *
+ */
+
+/*jshint esversion: 6 */
+
+const express           = require('express');
+const app               = express();
+const http              = require('http');
+const mongoose			    = require('mongoose');
+const bodyParser        = require('body-parser');
+const config            = require('./keys/configdb');
+
+require('./modules/dbconnection')(mongoose, config);
+
+const crud              = require('./modules/dbcrud')(mongoose);
+
+http.createServer(app).listen(3000);
+
+app.use(bodyParser.json());							          //for parsing application/json
+app.use(bodyParser.urlencoded({extended: true}));	// for parsing application/x-www-form-urlencoded
+
+app.disable('x-powered-by');
+
+// Routes
+require('./routes/dashboard.js')(app, crud);
+
+module.exports = app;
