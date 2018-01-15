1c31b1f1787bacdfea9d3724bacdd1b3db31c075
Fabio Lenine <fabiolenine@gmail.com>
2018-01-14 22:40:29 -0300

    add
    - Adicionado dois arquivos .sh para automatizar a instalação e execução
    da aplicação.

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo "Begin install NPM..."
+sudo apt-get install -y npm
+echo "Begin install AWS-SDK..."
+sudo npm install aws-sdk
+echo "Atualizar os pacotes para a versão 6.x do Nodejs..."
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+echo "Begin install Nodejs..."
+sudo apt-get install -y nodejs
+echo "Begin install Express, Mongoose , EJS and Body-parser..."
+npm install
+echo "Begin install PM2..."
+sudo npm install pm2@latest -g
+echo "Begin build-essential..."
+sudo apt-get install -y build-essential
+echo "Game over!"
---
+++ start.sh
@@ -0,0 +1,3 @@
+echo "Starts the URL Shortener using max CPUs..."
+sudo pm2 start wsconpass.js -i max
+echo "Check service status..."
